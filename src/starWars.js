b5b0855c3f2d5e9ba94030c1cf87422879bc624f
louiseuna <louiseuna15@gmail.com>
2023-01-10 22:31:49 +0000

    restructuring api call and setting up starships

--- src/starWars.js
+++ src/starWars.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import StarWarsComponent from "./StarWarsComponent.js";
 import "./App.css";
 
-function StarWars() {
+export default function StarWars(props) {
+  const [starship, setStarship] = useState(props.starship);
+
+  function starShips(response) {
+    setStarship({
+      ready: true,
+      setLoaded: true,
+      name: response.data.results[0].name,
+      model: response.data.results[0].model,
+      manufacturer: response.data.results[0].manufacturer,
+      cost: response.data.results[0].cost_in_credits,
+      length: response.data.results[0].length,
+      crew: response.data.results[0].crew,
+      pax: response.data.results[0].passengers,
+      cargo: response.data.results[0].cargo_capacity,
+    });
+  }
+  function submitStarship(event) {
+    event.preventDefault();
+    search();
+  }
+  function search() {
+    let dataUrl = `https://swapi.dev/api/starships?format=json`;
+    axios.get(dataUrl).then(starShips);
+  }
+  search();
   return (
-    <div>
-      starWars.js here
-      <StarWarsComponent />
+    <div className="form">
+      <div>
+        <label for="starwars" onChange={submitStarship}>
+          StarShips!
+          <input
+            className="radio"
+            type="radio"
+            value="starships"
+            name="starwars"
+          />
+        </label>
+        <label for="starwars">
+          Vehicles!
+          <input
+            className="radio"
+            type="radio"
+            value="vehicles"
+            name="starwars"
+          />
+        </label>
+        <label for="starwars">
+          Films!
+          <input className="radio" type="radio" value="films" name="starwars" />
+        </label>
+      </div>
+      <div>
+        starWars.js here
+        <StarWarsComponent data={starship} />
+      </div>
     </div>
   );
 }
-export default StarWars;
--- src/starWarsComponent.js
+++ src/starWarsComponent.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function StarWarsComponent() {
-  return <div>hello from starWarsComponent.js</div>;
-}
+// function StarWarsComponent() {
+//   return <div>hello from starWarsComponent.js</div>;
+// }
 
-export default StarWarsComponent;
+export default function StarWarsComponent(props) {
+  console.log(props.cost);
+  return (
+    <div className="container">
+      <div className="container-inner">
+        <h2 className="name">{props.data.name}</h2>
+      </div>
+    </div>
+  );
+}
